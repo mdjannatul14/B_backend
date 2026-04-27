ba9e8fc9c85b5aa01d01e238b291613b7c98314c
mdjannatul14 <mdjannatulferdous1411@gmail.com>
2026-04-27 08:51:36 -0500

    first code

---
+++ jannat1.py
@@ -0,0 +1,26 @@
+"""#dataa type
+#int data typy
+a = 10
+print(a)
+
+#flot data typy
+a = 3.33
+print(a)
+
+#range data type 
+number = range(0,19,3)
+print(*number)
+
+number2 = range(00,1000,200)
+print(*number2)"""
+
+sex = input("your sex point:")
+sex = int(sex)
+
+if sex >= 80:
+    print("med")
+elif 81 >= 100:
+    print("powerfull")  j
+else:
+    print("you are gay")
+
