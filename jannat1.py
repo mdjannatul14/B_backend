5175583223847d2f073658a8ab2611e05a70db12
mdjannatul14 <mdjannatulferdous1411@gmail.com>
2026-04-29 14:23:30 -0500

    leap year

--- jannat1.py
+++ jannat1.py
@@ -8,19 +8,44 @@ a = 3.33
 print(a)
 
 #range data type 
-number = range(0,19,3)
+number = range(0,100,2)
 print(*number)
 
 number2 = range(00,1000,200)
-print(*number2)"""
+print(*number2)
+
+#conditional lojic
 
 sex = input("your sex point:")
 sex = int(sex)
 
-if sex >= 80:
+if sex <= 80:
     print("med")
-elif 81 >= 100:
-    print("powerfull")  j
+elif 81 <= 100:
+    print("powerfull")
 else:
-    print("you are gay")
+    print("you are gay")"""
+
+#leap year
+
+"""year = input("year: ")
+year = int(year)
+
+if year % 4 == 0:  
+    if year % 100 == 0 and year % 400 == 0:
+        print( year, "is leap year")
+    elif year % 100 != 0:
+        print(year, "is leap year")
+    else:
+        print(year, "is not leap year")
+else:
+    print(year, "is not leap year")
+"""
+year = input("your year: ")
+year = int(year)
+
+if (year % 100 != 0 and year % 4 == 0) or (year % 400 == 0):
+    print(year, "is leap year")   
+else:
+    print("is not leap year")
 
